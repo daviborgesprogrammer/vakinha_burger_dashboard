099c4846de7bbc963af21b2ff6bc627602cbcb7f
Davi Borges <borges.davi.cezar@gmail.com>
2024-06-17 18:37:35 -0300

    tela de edição de tipo de pagamento

--- lib/src/modules/payment_type/payment_type_page.dart
+++ lib/src/modules/payment_type/payment_type_page.dart
@@ -6,6 +6,7 @@ import 'package:mobx/mobx.dart';
 import '../../core/ui/helpers/loader.dart';
 import '../../core/ui/helpers/messages.dart';
 import 'payment_type_controller.dart';
+import 'payment_type_form/payment_type_form_modal.dart';
 import 'widgets/payment_type_header.dart';
 import 'widgets/payment_type_item.dart';
 
@@ -25,6 +26,9 @@ class _PaymentTypePageState extends State<PaymentTypePage>
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      final filterDisposer = reaction((_) => controller.filterEnabled, (_) {
+        controller.loadPayments();
+      });
       final statusDisposer = reaction((_) => controller.status, (status) {
         switch (status) {
           case PaymentTypeStateStatus.initial:
@@ -41,11 +45,50 @@ class _PaymentTypePageState extends State<PaymentTypePage>
               controller.errorMessage ?? 'Erro ao buscar formas de pagamentos',
             );
             break;
+          case PaymentTypeStateStatus.addOrUpdatePayment:
+            hideLoader();
+            showAddOrUpdatePayment();
+            break;
+          case PaymentTypeStateStatus.saved:
+            hideLoader();
+            Navigator.of(context, rootNavigator: true).pop();
+            controller.loadPayments();
+            break;
         }
       });
-      disposers.addAll([statusDisposer]);
+      disposers.addAll([statusDisposer, filterDisposer]);
       controller.loadPayments();
     });
+  }
+
+  @override
+  void dispose() {
+    for (final dispose in disposers) {
+      dispose();
+    }
+    super.dispose();
+  }
+
+  void showAddOrUpdatePayment() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Material(
+          color: Colors.black26,
+          child: Dialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            backgroundColor: Colors.white,
+            elevation: 10,
+            child: PaymentTypeFormModal(
+              model: controller.paymentTypeSelected,
+              controller: controller,
+            ),
+          ),
+        );
+      },
+    );
   }
 
   @override
@@ -55,7 +98,9 @@ class _PaymentTypePageState extends State<PaymentTypePage>
       padding: const EdgeInsets.only(left: 40, top: 40, right: 40),
       child: Column(
         children: [
-          const PaymentTypeHeader(),
+          PaymentTypeHeader(
+            controller: controller,
+          ),
           const SizedBox(height: 50),
           Expanded(
             child: Observer(
@@ -69,7 +114,10 @@ class _PaymentTypePageState extends State<PaymentTypePage>
                 ),
                 itemBuilder: (context, index) {
                   final paymentTypeModel = controller.paymentTypes[index];
-                  return PaymentTypeItem(payment: paymentTypeModel);
+                  return PaymentTypeItem(
+                    payment: paymentTypeModel,
+                    controller: controller,
+                  );
                 },
               ),
             ),
--- lib/src/modules/payment_type/widgets/payment_type_item.dart
+++ lib/src/modules/payment_type/widgets/payment_type_item.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 
+import '../../../core/ui/styles/colors_app.dart';
 import '../../../core/ui/styles/text_styles.dart';
 import '../../../models/payment_type_model.dart';
+import '../payment_type_controller.dart';
 
 class PaymentTypeItem extends StatelessWidget {
+  final PaymentTypeController controller;
   final PaymentTypeModel payment;
-  const PaymentTypeItem({super.key, required this.payment});
+  const PaymentTypeItem({
+    super.key,
+    required this.payment,
+    required this.controller,
+  });
 
   @override
   Widget build(BuildContext context) {
+    final colorAll = payment.enabled ? Colors.black : Colors.grey;
     return Container(
       decoration: BoxDecoration(
         border: Border.all(color: Colors.grey),
@@ -20,32 +28,52 @@ class PaymentTypeItem extends StatelessWidget {
           children: [
             Image.asset(
               'assets/images/icons/payment_${payment.acronym}_icon.png',
-              errorBuilder: (context, error, stackTrace) =>
-                  Image.asset('assets/images/icons/payment_notfound_icon.png'),
+              errorBuilder: (context, error, stackTrace) => Image.asset(
+                'assets/images/icons/payment_notfound_icon.png',
+                color: colorAll,
+              ),
+              color: colorAll,
             ),
             const SizedBox(width: 20),
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Forma de Pagamento',
-                  style: context.textStyles.textRegular,
-                ),
-                const SizedBox(height: 20),
-                Text(
-                  payment.name,
-                  style: context.textStyles.textTitle,
-                ),
-              ],
+            Expanded(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  FittedBox(
+                    child: Text(
+                      'Forma de Pagamento',
+                      style: context.textStyles.textRegular
+                          .copyWith(color: colorAll),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  FittedBox(
+                    child: Text(
+                      payment.name,
+                      style: context.textStyles.textTitle
+                          .copyWith(color: colorAll),
+                    ),
+                  ),
+                ],
+              ),
             ),
             Expanded(
               child: Align(
                 alignment: Alignment.bottomRight,
                 child: TextButton(
-                  onPressed: () {},
-                  child: const Text('Editar'),
+                  onPressed: () {
+                    controller.editPayment(payment);
+                  },
+                  child: Text(
+                    'Editar',
+                    style: context.textStyles.textMedium.copyWith(
+                      color: payment.enabled
+                          ? context.colors.primary
+                          : Colors.grey,
+                    ),
+                  ),
                 ),
               ),
             ),

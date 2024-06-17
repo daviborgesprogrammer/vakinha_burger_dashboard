2706a72b6dbf3f55d56932ba51188d3f5c094e09
Davi Borges <borges.davi.cezar@gmail.com>
2024-06-17 11:34:01 -0300

    listagem de formas de pagamento

--- lib/src/modules/payment_type/payment_type_page.dart
+++ lib/src/modules/payment_type/payment_type_page.dart
@@ -1,10 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:mobx/mobx.dart';
 
+import '../../core/ui/helpers/loader.dart';
+import '../../core/ui/helpers/messages.dart';
+import 'payment_type_controller.dart';
 import 'widgets/payment_type_header.dart';
 import 'widgets/payment_type_item.dart';
 
-class PaymentTypePage extends StatelessWidget {
+class PaymentTypePage extends StatefulWidget {
   const PaymentTypePage({super.key});
+
+  @override
+  State<PaymentTypePage> createState() => _PaymentTypePageState();
+}
+
+class _PaymentTypePageState extends State<PaymentTypePage>
+    with Loader, Messages {
+  final controller = Modular.get<PaymentTypeController>();
+  final disposers = <ReactionDisposer>[];
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      final statusDisposer = reaction((_) => controller.status, (status) {
+        switch (status) {
+          case PaymentTypeStateStatus.initial:
+            break;
+          case PaymentTypeStateStatus.loading:
+            showLoader();
+            break;
+          case PaymentTypeStateStatus.loaded:
+            hideLoader();
+            break;
+          case PaymentTypeStateStatus.error:
+            hideLoader();
+            showError(
+              controller.errorMessage ?? 'Erro ao buscar formas de pagamentos',
+            );
+            break;
+        }
+      });
+      disposers.addAll([statusDisposer]);
+      controller.loadPayments();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,17 +58,20 @@ class PaymentTypePage extends StatelessWidget {
           const PaymentTypeHeader(),
           const SizedBox(height: 50),
           Expanded(
-            child: GridView.builder(
-              itemCount: 10,
-              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                mainAxisExtent: 120,
-                mainAxisSpacing: 20,
-                crossAxisSpacing: 10,
-                maxCrossAxisExtent: 680,
+            child: Observer(
+              builder: (_) => GridView.builder(
+                itemCount: controller.paymentTypes.length,
+                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                  mainAxisExtent: 120,
+                  mainAxisSpacing: 20,
+                  crossAxisSpacing: 10,
+                  maxCrossAxisExtent: 680,
+                ),
+                itemBuilder: (context, index) {
+                  final paymentTypeModel = controller.paymentTypes[index];
+                  return PaymentTypeItem(payment: paymentTypeModel);
+                },
               ),
-              itemBuilder: (context, index) {
-                return const PaymentTypeItem();
-              },
             ),
           ),
         ],
--- lib/src/modules/payment_type/widgets/payment_type_item.dart
+++ lib/src/modules/payment_type/widgets/payment_type_item.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 import '../../../core/ui/styles/text_styles.dart';
+import '../../../models/payment_type_model.dart';
 
 class PaymentTypeItem extends StatelessWidget {
-  const PaymentTypeItem({super.key});
+  final PaymentTypeModel payment;
+  const PaymentTypeItem({super.key, required this.payment});
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +18,11 @@ class PaymentTypeItem extends StatelessWidget {
         padding: const EdgeInsets.all(20.0),
         child: Row(
           children: [
-            Image.asset('assets/images/icons/payment_cc_icon.png'),
+            Image.asset(
+              'assets/images/icons/payment_${payment.acronym}_icon.png',
+              errorBuilder: (context, error, stackTrace) =>
+                  Image.asset('assets/images/icons/payment_notfound_icon.png'),
+            ),
             const SizedBox(width: 20),
             Column(
               mainAxisSize: MainAxisSize.min,
@@ -29,7 +35,7 @@ class PaymentTypeItem extends StatelessWidget {
                 ),
                 const SizedBox(height: 20),
                 Text(
-                  'Cartão de Crédito',
+                  payment.name,
                   style: context.textStyles.textTitle,
                 ),
               ],
